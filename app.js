a4e440adf182f3cf3bfab581bf22ea815f9cc755
Nicholas Carr <nicholas.carr@rogers.com>
2017-10-21 23:23:51 -0400

    Actually understood how callbacks/promises work

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ const http = require('http')
 const express = require('express')
 const MessagingResponse = require('twilio').twiml.MessagingResponse
 const bodyParser = require('body-parser')
+const summarizer = require('./summarizer')
 
 const app = express()
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse()
-
-  console.log(req.body.Body)
-  twiml.message('The Robots are coming! Head for the hills!')
-
-  res.writeHead(200, {'Content-Type': 'text/xml'})
-  res.end(twiml.toString())
+  summarizer(req.body.Body)
+  .then(result => twiml.message(result))
+  .then(() => res.writeHead(200, {'Content-Type': 'text/xml'}))
+  .then(() => res.end(twiml.toString()))
 });
 
 http.createServer(app).listen(process.env.PORT || 3000, () => {
--- summarizer.js
+++ summarizer.js
@@ -1,27 +1,31 @@
 function summarize(input) {
 
-input = input.replace(/ /g,"_");
+	input = input.replace(/ /g,"_");
 
-const SMMRY_API_KEY = "C5395F3DC0";
-const unirest = require("unirest");
-const SM_LENGTH = 2;
-const SM_API_KEY = "C5395F3DC0";
-const SM_URL = "https://simple.wikipedia.org/wiki/" + input;
+	const axios = require("axios");
+	const SM_LENGTH = 2;
+	const SM_API_KEY = "C5395F3DC0";
+	const SM_URL = "https://simple.wikipedia.org/wiki/" + input;
 
+	let output
 
-
-unirest.get("https://community-smmry.p.mashape.com/?SM_API_KEY=" + SM_API_KEY + 
-	"&SM_LENGTH=" + SM_LENGTH + "&SM_URL=" + SM_URL)
-
-.header("X-Mashape-Key", "f7l5sOE3wImshRGFoYCMv3gLU72dp1ntKPCjsn7vwZZeVaECJK")
-.header("Accept", "text/plain")
-.end(function (result) {
-	let output = result.body.sm_api_content;
-	if (output === undefined) {
-		output = "Unfortunately, I wasn't able to find information on that subject.";
-	}
-  return output;
-});
-
+	return axios.get("https://community-smmry.p.mashape.com/", {
+		params: {
+			SM_API_KEY,
+			SM_LENGTH,
+			SM_URL
+		},
+		headers: {
+			'X-Mashape-Key': 'f7l5sOE3wImshRGFoYCMv3gLU72dp1ntKPCjsn7vwZZeVaECJK',
+			'Accept': 'text/plain'
+		}
+	})
+	.then(result => {
+		output = result.data.sm_api_content;
+		if (output === undefined) {
+			output = "Unfortunately, I wasn't able to find information on that subject.";
+		}
+		return output
+	})
 }
 module.exports = summarize;
