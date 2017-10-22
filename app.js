9b1fe133d5163b52b4211b524c077fb418db6130
Nicholas Carr <nicholas.carr@rogers.com>
2017-10-21 21:52:40 -0400

    Added twilio api

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require('http')
+const express = require('express')
+const MessagingResponse = require('twilio').twiml.MessagingResponse
+
+const app = express()
+
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse()
+
+  console.log(req.body.Body)
+  twiml.message('The Robots are coming! Head for the hills!')
+
+  res.writeHead(200, {'Content-Type': 'text/xml'})
+  res.end(twiml.toString())
+});
+
+http.createServer(app).listen(process.env.PORT || 3000, () => {
+  console.log('Express server listening on port ' + (process.env.PORT || 3000))
+})
